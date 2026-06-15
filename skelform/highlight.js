f8783b739ddeed30fe10f399a836d98d937b4e3f
Retropaint <darkglasses1122@gmail.com>
2026-06-15 16:21:54 +0700

    added true and false to non-var keywords

--- skelform/highlight.js
+++ skelform/highlight.js
@@ -3240,7 +3240,7 @@ if (typeof exports === 'object' && typeof module !== 'undefined') { module.expor
         ),
         end: IDENT_RE$1,
         // built-in keywords that should not be considered variables
-        keywords: ["let", "break", "continue", "const"],
+        keywords: ["let", "break", "continue", "const", "true", "false"],
         className: "variable",
         relevance: 1
       };
